83390b13640363f58e26aefc090232a49f407f85
Michael Kruszewski <mdkruszewski@gmail.com>
2018-01-28 17:34:19 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,7 +19,7 @@ function pressIt() {
 }
 
 function submitIt() {
-  $('#typing').on('submit', function() {
+  $(':submit').on('submit', function() {
     alert('Your form is going to be submitted now.')
   })
 }
