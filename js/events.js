4ef7a84f3165563b359b8d0ad42b88e6a6fd4e8e
Michael Kruszewski <mdkruszewski@gmail.com>
2018-01-28 17:22:55 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,8 +4,13 @@ function getIt() {
   })
 }
 
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
 $(document).ready(function(){
   getIt()
-
-
-});
+  frameIt()
+})
