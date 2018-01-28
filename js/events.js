54ce92033c55343f60c8243edb67711f336836d6
Michael Kruszewski <mdkruszewski@gmail.com>
2018-01-28 17:13:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,12 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+    return
+  })
+}
 
 $(document).ready(function(){
-
+  getIt()
 // call functions here
 
 });
